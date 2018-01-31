d1e6ecd367c46193831724dd0094070e6c2402ab
An Nuyen <annuyen@gmail.com>
2018-01-31 16:03:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,19 @@ class Driver {
   }
 }
 
+let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+
 class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation; 
+  }
+
+  blocksTravelled() {
+    let startPoint = eastWest.indexOf(this.beginningLocation['horizontal'])
+    let endPoint = eastWest.indexOf(this.endingLocation['horizontaol'])
+    let vertical = Math.abs(this.endingLocation['vertical'] - this.beginningLocation['vertical'])
+    return Math.abs(endPoint - startPoint) + vertical
+  }
 
 }
