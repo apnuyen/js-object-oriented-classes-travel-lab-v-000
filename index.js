72f090181ce267a75face99d18e8089f745577e1
An Nuyen <annuyen@gmail.com>
2018-01-31 16:09:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,9 +18,9 @@ class Route {
   }
 
   blocksTravelled() {
-    let startPoint = eastWest.indexOf(this.beginningLocation['horizontal']);
-    let endPoint = eastWest.indexOf(this.endingLocation['horizontaol']);
-    let vertical = Math.abs(this.endingLocation['vertical'] - this.beginningLocation['vertical']);
+    let startPoint = eastWest.indexOf(this.beginningLocation.horizontal);
+    let endPoint = eastWest.indexOf(this.endingLocation.horizontal);
+    let vertical = Math.abs(this.endingLocation.vertical - this.beginningLocation.vertical);
     return Math.abs(endPoint - startPoint) + vertical;
   }
 
